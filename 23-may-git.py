bc3bc95ef5736e144d44f779513fda5c64d9e2fc
honor-AJ <jeffrey.ntim@gmail.com>
2022-05-23 22:21:43 +0200

    adding data types

--- 23-may-git.py
+++ 23-may-git.py
@@ -1,2 +1,30 @@
 print("Hello world")
+x = 1
+print(x)
+x = "hello world"
+print(x)
+x = 1.2
+print(x)
+x = list("applebread")
+print(x)
+x = [1,2,3,4]
+print(x)
+x = ["apple"]
+print(x)
+x = set("apple")
+print(x)
+x = (1,2)
+print(x)
+x = range(5)
+print(x)
 
+for i in range(5):
+    print(i)
+x = bool(-1)
+print(x)
+x=bool(1)
+print(x)
+x=bool(0)
+print(x)
+x=True
+print(x)

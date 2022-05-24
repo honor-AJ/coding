d7543f0c413e5cec64d3923d9852074f7455bba0
honor-AJ <jeffrey.ntim@gmail.com>
2022-05-24 20:47:17 +0200

    lesson2

---
+++ 24-may-flow.py
@@ -0,0 +1,28 @@
+for i in range(10):
+    print(i)
+
+for i in "apple":
+    print(i)
+
+for i in ["apple"]:
+    print(i)
+
+i=0
+# while i <10:
+    # print(i)
+
+while False:
+    print("apple")
+
+i=20
+while i <10:
+    print(i)
+i=0
+while i<100:
+    print(i)
+    i=i+1
+
+w=True
+while w:
+    print("apple")
+    w=False
